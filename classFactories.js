52d4ed1a6052450bc4eed5f16045111bd300b8ee
TH876 <tayhiggins14@gmail.com>
2023-03-15 13:39:29 -0400

    init

---
+++ classFactories.js
@@ -0,0 +1,14 @@
+/*Creating Classes & Factories
+Hamster
+attributes:
+
+owner - string, initially set as an empty string
+name - string, set the name from parameter in constructor method
+price - integer, set as 15
+methods:
+
+wheelRun() - log "squeak squeak"
+eatFood() - log "nibble nibble"
+getPrice() - return the price
+🔴 Hard Mode Save & Commit your work!
+*/
